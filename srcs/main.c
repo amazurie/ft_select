4a3d8a790ee86043684aad637aa99b847c5ccd2b
amazurie <amazu@live.fr>
2017-06-29 14:13:54 +0200

    up TERM load

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: amazurie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/11 11:11:21 by amazurie          #+#    #+#             */
-/*   Updated: 2017/06/29 12:40:51 by amazurie         ###   ########.fr       */
+/*   Updated: 2017/06/29 14:13:37 by amazurie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,12 +44,11 @@ int			main(int ac, char **av)
 		disp_error(
 				"ft_select: bad input\nusage: /ft_select input [input...]\n");
 	if (!(name_term = getenv("TERM")))
-		disp_error("No terminal specified, specify it with: 'setenv TERM'\n");
+		name_term = "xterm";
+	if (tgetent(NULL, name_term) == ERR || tcgetattr(0, &d->oldterm) == -1)
+		disp_error("could not load TERM! specify it with: 'setenv TERM'\n");
 	if (!(d = (t_data *)ft_memalloc(sizeof(t_data))))
 		disp_error("malloc");
-	if ((tgetent(NULL, name_term) == ERR && tgetent(NULL, "xterm") == ERR)
-			|| tcgetattr(0, &d->oldterm) == -1)
-		disp_error(NULL);
 	tcgetattr(0, &d->term);
 	d->term.c_lflag &= ~(ECHO | ICANON);
 	d->term.c_cc[VMIN] = 1;
